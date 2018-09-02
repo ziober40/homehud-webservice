f318a840a8ef59061c7cffd5d701c9fbca9b7ff7
Ziobertini <ziobrowski.bartek@gmail.com>
2018-09-02 17:05:27 +0100

    certificate and basic command uploaded

---
+++ app.js
@@ -0,0 +1,18 @@
+var fs = require('fs'); 
+var https = require('https'); 
+var options = { 
+    key: fs.readFileSync('server-key.pem'), 
+    cert: fs.readFileSync('server-crt.pem'), 
+    ca: fs.readFileSync('ca-crt.pem'), 
+    requestCert: true, 
+    rejectUnauthorized: true
+}; 
+https.createServer(options, function (req, res) { 
+    console.log(new Date()+' '+ 
+        req.connection.remoteAddress+' '+ 
+        req.socket.getPeerCertificate().subject.CN+' '+ 
+        req.method+' '+req.url); 
+    res.writeHead(200); 
+    res.end(JSON.stringify({ "version": "1.0", "response": { "outputSpeech": { "type": "PlainText", "text": "hello hello hello yo yo yo world!"} }}));
+    //res.end("hello world\n"); 
+}).listen(8000);
---
+++ client.js
@@ -0,0 +1,19 @@
+var fs = require('fs'); 
+var https = require('https'); 
+var options = { 
+    hostname: 'localhost', 
+    port: 8000, 
+    path: '/', 
+    method: 'GET', 
+    key: fs.readFileSync('client2-key.pem'), 
+    cert: fs.readFileSync('client2-crt.pem'), 
+    ca: fs.readFileSync('ca-crt.pem') }; 
+var req = https.request(options, function(res) { 
+    res.on('data', function(data) { 
+        process.stdout.write(data); 
+    }); 
+}); 
+req.end(); 
+req.on('error', function(e) { 
+    console.error(e); 
+});
